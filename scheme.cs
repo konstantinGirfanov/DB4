6c1d33a1d381983482aba5fb5c473917afef36e4
Kostya <super_serega_2007@mail.ru>
2023-03-22 23:59:32 +0500

    scheme class

---
+++ scheme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scheme
+{
+    class Scheme
+    {
+        public string Name { get; set; }
+        public List<SchemeColumn> Columns { get; set; }
+    }
+
+    class SchemeColumn
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
