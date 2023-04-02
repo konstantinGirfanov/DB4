9cf5b13b32d7f0441ca54d8f3417cbcf011df463
Kostya <super_serega_2007@mail.ru>
2023-04-03 02:04:27 +0500

    жестко почистил полные файлы кода

--- workWithFiles.cs
+++ workWithFiles.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-namespace DB4
+﻿namespace DB4
 {
     static class WorkWithFiles
     {
