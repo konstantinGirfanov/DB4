08e8bd923d5b1236839bedf341d23f60dd64b046
Kostya <super_serega_2007@mail.ru>
2023-04-03 01:13:10 +0500

    схемы и данные теперь считываются из соответстующих папок

--- Program.cs
+++ Program.cs
@@ -9,13 +9,14 @@ namespace DB4
             Console.WriteLine("Введите название схемы (например readers.json):");
             var schemeName = Console.ReadLine();
 
-            var pathScheme = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\" + schemeName;
+            var pathScheme = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\" + "schemas" + "\\" + schemeName;
+
             Scheme scheme = WorkWithScheme.ReadScheme(pathScheme);
 
             Console.WriteLine("Введите название файла данных для схемы (например readersData.txt):");
             string dataName = Console.ReadLine();
 
-            string pathData = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\" + dataName;
+            string pathData = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\" + "data" + "\\" + dataName;
             SchemeData readersData = new(scheme, pathData);
 
             readersData.PrintData();
